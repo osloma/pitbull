80ca2b340145d6963c5c9d0e0de1eac6f68f181f
oscar lopez <oscar.lopez.martinez@gmail.com>
2020-11-26 21:28:41 +0000

    Fixed removal of last rows as it's a disclaimer

--- data/extract/ark/ark_download.py
+++ data/extract/ark/ark_download.py
@@ -14,10 +14,12 @@ def load_data():
     for f in funds:
         url = f"https://ark-funds.com/wp-content/fundsiteliterature/csv/{f}.csv"
         s = requests.get(url).content
-        print(f"Downloading file {f}")
-        data = pd.read_csv(io.StringIO(s.decode('utf-8'))).dropna()
+        print(f"Downloading file {f} from {url}")
+        data = pd.read_csv(io.StringIO(s.decode('utf-8')))
+        data = data[data.company.notnull()]
         data["fecha"] = pd.to_datetime(data["date"], format="%m/%d/%Y")
         date = data.fecha.head().iloc[0].strftime('%Y%m%d')
+        print(f"For date {date}")        
         path = f"/pitbull/download/{f}-{str(date)}.csv"
         data.to_csv(path_or_buf = path, index=False)
 
