707679c9782520df205c1bfcfc1512765f11e08b
oscar lopez <oscar.lopez.martinez@gmail.com>
2020-12-09 22:34:33 +0000

    added real time prices

--- data/analysis/ark/compare.py
+++ data/analysis/ark/compare.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 from os import listdir
 from os.path import abspath, isfile, join
+from yahoo_fin import stock_info as si
 import sys, os
 sys.path.append(".")
 from data.comon import constants
@@ -12,7 +14,7 @@ Important note:
 
 class DataCompare():
     __path = os.path.abspath(constants.download_path)
-    __colums_to_select = ["ticker", "shares", "weight_perc"]
+    __colums_to_select = ["ticker", "company", "shares", "weight_perc"]
 
     def __init__(self, fund):
         self.fund = fund
@@ -23,9 +25,11 @@ class DataCompare():
         """
         onlyfiles = [f for f in listdir(self.__path) if isfile(join(self.__path, f))]
         raw_dates = [x[x.index("-")+1:].replace(".csv", "") for x in onlyfiles]
-        return list(set(map(int, raw_dates)))
+        
 
-    def __sort_df_columns(self, df):
+        return sorted(list(set(map(int, raw_dates))), reverse=True)
+
+    def __sort_df_columns_alpha(self, df):
         """
         Sorts a dataframe on its columns based on their name alphabetically
         """
@@ -43,15 +47,36 @@ class DataCompare():
 
         joined = previous_data.merge(current_data, on = "ticker", how = "right", suffixes=("_prev", "_curr"))
 
-        return self.__sort_df_columns(joined)
+        return self.__sort_df_columns_alpha(joined)
 
     def __differences_interday(self, data):
         """
         Calculates differences between columns shares and weight in dataframes
         """
-        data["weight_per_diff"] = data["weight_perc_curr"] - data["weight_perc_prev"]    
-        data["shares_diff"] = data["shares_curr"] - data["shares_prev"]
-        return self.__sort_df_columns(data)
+        data["weight_perc_diff"] = data["weight_perc_curr"] - data["weight_perc_prev"]    
+        data["shares_diff"] = (data["shares_curr"] - data["shares_prev"])/data["shares_prev"]*100
+        prices = self.__get_stocks_price(data)
+        data = data.merge(prices, on = 'ticker', how = "left")
+
+        col_rename = {'shares_diff': 'SHARES(%)', 'ticker': 'TICKER', 'shares_curr': 'SHARES', 
+            'weight_perc_curr': 'WEIGHT', 'weight_perc_diff': 'WEIGHT(%)', 'company_curr': 'COMPANY', 'price': 'PRICE'}
+        col_output = ['COMPANY','TICKER', 'SHARES(%)', 'WEIGHT(%)', 'PRICE', 'WEIGHT', 'SHARES']
+
+        return self.__sort_df_columns_alpha(data).rename(columns=col_rename)[col_output]
+
+    def __get_stocks_price(self, df):
+        """
+        Obtains real time stock prices 
+        """
+        tickers = df['ticker'].tolist()
+        print(tickers)
+        prices = pd.DataFrame(columns = ['ticker', 'price'])
+        for t in tickers:
+            if t == t:
+                price = si.get_live_price(t.split(' ')[0])
+                prices = prices.append({'ticker': t, 'price': price}, ignore_index=True)
+        return prices
+
 
     def obtain_differences(self, previous_date, current_date):
         print("Starting downloading process")    
--- data/analysis/ark/report.py
+++ data/analysis/ark/report.py
@@ -18,24 +18,60 @@ class DashBoard():
         return selected_metrics
 
     def __add_dates(self, fund: str):
+        values = DataCompare(fund).available_dates()
+        default_previous = values.index(values[1])
         previous_date = st.sidebar.selectbox(
             label="Previous date",
-            options=DataCompare(fund).available_dates()
+            options=values,
+            index=default_previous
         )
         current_date = st.sidebar.selectbox(
             label="Current date",
-            options=DataCompare(fund).available_dates()
+            options=values
         )
         return previous_date, current_date
 
+#https://mode.com/example-gallery/python_dataframe_styling/
+
     def __add_table(self, previous_date: str, current_date: str, fund: str):
         data = DataCompare(fund).obtain_differences(previous_date=previous_date, current_date=current_date)
-        st.dataframe(data)
+        st.dataframe(data.style.highlight_max(axis=0))
+
+    def __add_title(self):
+        st.markdown("""
+        ### Note: 
+        - I takes time to download prices as it needs to go to Yahoo Finance web service. Will improve in the future with other WS
+        - Highlighted in yellow the max for weight and shares
+        - SHARES(%) and WEIGHT(%) is the percentage variation from previous to current date on them
+        - WEIGHT and SHARES are values for current date
+        """)
+        st.markdown(
+        f"""
+            <style>
+                .reportview-container .main .block-container{{                    
+                    padding-top: 1rem;
+                    padding-right: 1rem;
+                    padding-left: 1rem;
+                    padding-bottom: 1rem;
+                    width:1700px;
+                }}
+                .reportview-container .main {{
+                    color: black;
+                    background-color: white;                    
+                }}
+                ul {{
+                    color:green;                    
+                }}
+            </style>
+            """,
+                    unsafe_allow_html=True,
+                )
 
     def run(self):        
         fund = self.__add_side_panel()
-        previos_date, current_date = self.__add_dates(fund=fund)
+        previos_date, current_date = self.__add_dates(fund=fund)        
         self.__add_table(previous_date=previos_date, current_date=current_date, fund=fund)
+        self.__add_title()
 
 
 if __name__ == "__main__":
